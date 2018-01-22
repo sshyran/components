ddc3354599f167714c05fb77f8a642f0bc6c89fc
Gaelle MOURARET <gaellemouraret@gmail.com>
2018-01-22 16:44:06 +0100

    add create-state & tests

--- packages/@coorpacademy-progression-engine/src/create-state.js
+++ packages/@coorpacademy-progression-engine/src/create-state.js
@@ -1,10 +1,36 @@
 // @flow
-import type {NewProgression, State} from './types';
+import type {NewProgression, State, Engine} from './types';
+import getConfig from './config';
+import updateState from './update-state';
+import answerAction from './reducers/test/fixtures/actions';
 
 const createState = (progression: NewProgression): State => {
   // TODO Implement function
   // $FlowFixMe
-  return null;
+  const config = getConfig({ref: progression.engine.ref, version: progression.engine.version});
+
+  const initialState: State = {
+    lives: config.lives,
+    livesDisabled: config.livesDisabled,
+    isCorrect: true,
+    slides: [],
+    stars: 0,
+    requestedClues: [],
+    viewedResources: [],
+    step: {
+      current: 1
+    },
+    remainingLifeRequests: config.remainingLifeRequests,
+    hasViewedAResourceAtThisStep: false,
+    allAnswers: [],
+    variables: {}
+  };
+
+  //2 pour chacune des actions de progression, appeler l'updateState
+  const state = updateState(progression.engine, initialState, progression.actions);
+  console.log('state: ', state);
+
+  return state;
 };
 
 export default createState;
--- packages/@coorpacademy-progression-engine/src/test/create-state.js
+++ packages/@coorpacademy-progression-engine/src/test/create-state.js
@@ -1,9 +1,39 @@
 // @flow
 import test from 'ava';
 import createState from '../create-state';
+import answerAction from '../reducers/test/fixtures/actions';
 
 test('should...', t => {
   // TODO implement
   // $FlowFixMe
-  t.deepEqual(createState(null), {});
+  t.deepEqual(createState({
+    "engine": {
+      "ref": "microlearning",
+      "version": "1"
+    },
+    "content": {
+      "ref": "5.C7",
+      "type": "chapter"
+    },
+    "actions": [
+      "answerAction"
+    ]
+    }), {
+      lives: 1,
+      livesDisabled: false,
+      content: undefined,
+      nextContent: undefined,
+      isCorrect: true,
+      slides: [],
+      stars: 0,
+      requestedClues: [],
+      viewedResources: [],
+      step: {
+        current: 1
+      },
+      remainingLifeRequests: 1,
+      hasViewedAResourceAtThisStep: false,
+      allAnswers: [],
+      variables: {}
+  });
 });
