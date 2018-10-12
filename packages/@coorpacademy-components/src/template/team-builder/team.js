1e6e069a9e27ba276e05629f23c9cf95842aacbc
Silouane <silouane.galinou@efrei.net>
2018-10-12 10:43:14 +0200

    initial + color for the winner window

--- packages/@coorpacademy-components/src/template/team-builder/team.js
+++ packages/@coorpacademy-components/src/template/team-builder/team.js
@@ -8,7 +8,7 @@ import Member from './member';
 import style from './team.css';
 
 const Team = (props, context) => {
-  const {name, members, number} = props;
+  const {name, members, initial, number} = props;
   return (
     <div className={style.team}>
       <header
@@ -21,11 +21,15 @@ const Team = (props, context) => {
       <div className={style.members}>
         {map(member => {
           if (isEmpty(member)) return null;
+<<<<<<< 85b9a5d4296991ed0e5cd17c7a8fb8fd2062d0a5
 <<<<<<< d09599dd9a63193062fd18469a857ec04ede2372
           return <Member key={member.name} {...member} color={colors[number]} />;
 =======
           return <Member key={member.name} number={number} {...member} />;
 >>>>>>>  add  avatar Initial + color to the selected-team
+=======
+          return <Member key={member.name} intiial={initial} number={number} {...member} />;
+>>>>>>>  initial + color for the winner window
         }, members)}
       </div>
     </div>
